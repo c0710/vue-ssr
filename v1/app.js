ba1497809dd65c2e78744b5bf59ce3878b5dd753
c0710@foxmail.com <3038856.c>
2018-04-03 16:47:36 +0800

    init template

---
+++ v1/app.js
@@ -0,0 +1,28 @@
+const Vue = require('vue')
+const server = require('express')()
+const path = require('path')
+const resolve = file => path.resolve(__dirname, file)
+const createRenderer = require('vue-server-renderer').createRenderer
+const renderer = createRenderer({
+  template: require('fs').readFileSync(resolve('./index.template.html'), 'utf-8')
+})
+
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>访问的 URL 是： {{ url }}</div>`
+  })
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      console.log(err)
+      res.status(500).end('Internal Server Error!')
+      return
+    }
+    console.log(html)
+  })
+})
+
+server.listen(8001)
