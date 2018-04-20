a52e8eaf42c951cbcaf695464146b3a03125922e
Evannn <c0710@foxmail.com>
2018-04-20 17:33:05 +0800

    webpack.base.config公共配置

---
+++ v5/build/webpack.base.config.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    devtool: '#cheap-module-source-map',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist/',
+        filename: '[name]-[chunk-hash].js'
+    },
+    resolve: {
+        alias: {
+            'public': path.resolve(__dirname, '../public'),
+            'components': path.resolve(__dirname, '../src/componenets'),
+            '@': path.resolve(__dirname, '../src')
+        },
+        extensions: ['.js', '.vue']
+    },
+    module: {
+        noParse: /es6-promise\.js$/,
+        rules: [
+            {
+                test: /\.vue$/,
+                use: {
+                    loader: 'vue-loader',
+                    options: {
+                        preserveWhiteSpace: false,
+                        postcss: [
+                            require('autoprefixer')({
+                                browser: ['last 3 versions']
+                            })
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'img/[name].[hash:7].[ext]'
+                    }
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        name: 'fonts/[name].[hash:7].[ext]'
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.json/,
+                use: 'json-loader'
+            }
+        ]
+    },
+    performance: {
+        maxEntrypointSize: 300000,
+        hints: 'warning'
+    },
+    plugins: [
+        new webpack.optimize.Plugin({
+            compress: {warnings: false}
+        }),
+        new ExtractTextPlugin({
+            filename: 'common.[chunkhash].css'
+        })
+    ]
+}
