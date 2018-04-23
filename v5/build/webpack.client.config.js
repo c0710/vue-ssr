81f32ffa20a605746c2b6c00deeb7847888eeb58
Evannn <c0710@foxmail.com>
2018-04-23 10:06:21 +0800

    v5 webpack.server.config

--- v5/build/webpack.client.config.js
+++ v5/build/webpack.client.config.js
@@ -2,10 +2,34 @@ const path = require('path')
 const webpack = require('webpack')
 const merge = require('webpack-merge')
 const base = require('./webpack.base.config')
+const VueSSRClientPlugin = require('vue-server-renderer/client-plugin')
 
 const config = merge(base, {
     entry: {
         app: '../src/client.entry.js'
     },
-
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+            'process.env.VUE_ENV': '"client"',
+            'process.env.DEBUG_API': '"true"'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                return (
+                    /node_modules/.test(module.context) && !/\.css$/.test(module.require)
+                )
+            }
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest'
+        }),
+        // 这是将服务器的整个输出
+        // 构建为单个 JSON 文件的插件。
+        // 默认文件名为 `vue-ssr-server-bundle.json`
+        new VueSSRClientPlugin()
+    ]
 })
+
+module.exports = config
