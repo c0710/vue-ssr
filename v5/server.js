dd0944cbb5f6e597dadf8be48b67639e62e46d3e
Evannn <c0710@foxmail.com>
2018-04-26 11:29:18 +0800

    v5 配置server

--- v5/server.js
+++ v5/server.js
@@ -24,4 +24,34 @@ function createRenderer(bundle, options) {
 }
 
 let renderer
-// const bundle = require('./dist/vue-ssr-client-manifest')
+const bundle  = require('./dist/vue-ssr-server-bundle.json')
+const clientManifest  = require('./dist/vue-ssr-client-manifest.json')
+renderer = createRenderer(bundle, {
+    clientManifest
+})
+
+app.use(express.static('dist'))
+app.use(express.static('public'))
+
+function render(req, res) {
+    console.log(renderer)
+    const context = {
+        title: 'Vue Ssr 2.3',
+        url: 'xxx'
+    }
+    renderer.renderToString(context, (err, html) => {
+        if (err) {
+            console.log('err!')
+            console.log(err)
+            throw err
+        }
+        res.send(html)
+    })
+}
+
+app.get('/', render)
+
+const port = process.env.PORT || 8086
+app.listen(port, '0.0.0.0', () => {
+    console.log(`server started at localhost:${port}`)
+})
